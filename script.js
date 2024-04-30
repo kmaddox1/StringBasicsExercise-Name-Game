b5411884883bc9528798f8e68534b1a3a5d9d4e3
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 22:49:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName ="Kimberly";
+let middleName = "Nicole";
+let lastName = "Maddox";
+
+// concatenation 
+let fullName = firstName + " " + middleName + " " + lastName;
+console.log("Hello " + fullName + "!");
+
+// interpolation
+    
